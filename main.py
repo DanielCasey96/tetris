890e00fa974c134e3405bfd16db3baaa71c4fbc6
Daniel Casey <dan_spooner15@hotmail.co.uk>
2024-04-05 14:30:44 +0100

    change screen size, add quit and run

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from game import TetrisGame
 
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((300, 600))
+    screen = pygame.display.set_mode((800, 600))  # Change the screen size here
     pygame.display.set_caption("Tetris")
 
-    TetrisGame.start_game(screen)
+    game = TetrisGame(screen)
+    game.run()
+
+    pygame.quit()
+
 
 if __name__ == "__main__":
     main()
